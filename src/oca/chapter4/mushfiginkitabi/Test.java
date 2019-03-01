3c1e5dc187fc873a9f83f6e5c2812730f7c351d0
polad1997 <alqayev1997@gmail.com>
2019-03-01 17:16:17 +0400

    Chapter4 -> Mushfig 40%

---
+++ src/oca/chapter4/mushfiginkitabi/Test.java
@@ -0,0 +1,12 @@
+package oca.chapter4.mushfiginkitabi;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+
+    String method(int a) {
+        if (a > 0) return "";
+        return "";
+    }
+}
---
+++ src/oca/chapter4/mushfiginkitabi/accesmodifiers/father/Father.java
@@ -0,0 +1,9 @@
+package oca.chapter4.mushfiginkitabi.accesmodifiers.father;
+
+public class Father {
+    protected String nameFather = "father";
+
+    protected void fatherMethod() {
+        System.out.println("im father");
+    }
+}
