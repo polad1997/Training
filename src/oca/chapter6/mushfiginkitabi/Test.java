dbad4f396913e2d3be00b3e86bc748bee7b2233c
Polad Alqayev <alqayev1997@gmail.com>
2019-04-01 00:55:04 +0400

    Chapter6 -> Mushfig 50%

---
+++ src/oca/chapter6/mushfiginkitabi/Test.java
@@ -0,0 +1,13 @@
+package oca.chapter6.mushfiginkitabi;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+
+    public void metod() {
+        throw new RuntimeException();
+//        throw new Exception();  need to try checked exceptions
+    }
+
+}
