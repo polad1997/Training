85e4c6fda149d6c7b1e530654790309d3ff7d411
polad1997 <alqayev1997@gmail.com>
2019-04-05 19:31:46 +0400

    updating

---
+++ src/oca/chapter5/interfaces/Herbivore.java
@@ -0,0 +1,7 @@
+package oca.chapter5.interfaces;
+
+public interface Herbivore {
+    String name = "Omnivore";
+    public void eat();
+
+}
--- src/oca/chapter5/interfaces/Test.java
+++ src/oca/chapter5/interfaces/Test.java
@@ -1,4 +1,7 @@
 package oca.chapter5.interfaces;
 
-public interface Test {
+public class Test implements Herbivore , Omnivore {
+    @Override
+    public void eat() {
+    }
 }
